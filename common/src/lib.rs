1b41812f8a8bfcc13ae1f6b19c414a407890a83f
0x40404040 <>
2019-12-26 18:10:22 +0100

    moved day_02:intcode_computer into common lib for day_05

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,27 +1,29 @@
+pub mod intcode;
+
 /// Module to help with the 2 part distinction in advent of code  
 pub mod part {
-	/// returns one or two depending on the FIRST command line arugment
-	/// exit(1) when no or wrong CLI argument is found
-	/// CLI -- 1 returns one
-	/// CLI -- 2 returns two
-	pub fn one_or_two<T>(one: T, two: T) -> T {
-		let arg1 = std::env::args().nth(1).unwrap_or_default();
-		match arg1.as_ref() {
-			"1" => one,
-			"2" => two,
-			_ => {
-				eprintln!("First CLI argument has to be 1 or 2");
-				std::process::exit(1);
-			}
-		}
-	}
+    /// returns one or two depending on the FIRST command line arugment
+    /// exit(1) when no or wrong CLI argument is found
+    /// CLI -- 1 returns one
+    /// CLI -- 2 returns two
+    pub fn one_or_two<T>(one: T, two: T) -> T {
+        let arg1 = std::env::args().nth(1).unwrap_or_default();
+        match arg1.as_ref() {
+            "1" => one,
+            "2" => two,
+            _ => {
+                eprintln!("First CLI argument has to be 1 or 2");
+                std::process::exit(1);
+            }
+        }
+    }
 
-	/// returns true if part one is given as command line argument
-	pub fn is_part_one() -> bool {
-		one_or_two(true, false)
-	}
+    /// returns true if part one is given as command line argument
+    pub fn is_part_one() -> bool {
+        one_or_two(true, false)
+    }
 }
 
 pub mod reexport {
-	pub use itertools;
+    pub use itertools;
 }
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::read_to_string;
 use std::io;
 
+use common::intcode::Computer;
 use common::part::one_or_two;
 use common::reexport::itertools::Itertools;
 
@@ -43,102 +44,4 @@ fn run() -> io::Result<()> {
 	}
 
 	Ok(())
-}
-
-struct Computer {
-	rom: Vec<usize>,
-	memory: Vec<usize>,
-	ip: usize,
-}
-
-impl From<Vec<usize>> for Computer {
-	fn from(v: Vec<usize>) -> Self {
-		Computer {
-			rom: v.clone(),
-			memory: v,
-			ip: 0,
-		}
-	}
-}
-
-impl Computer {
-	// convenient function for testing purpose only
-	#[allow(unused)]
-	fn run_memory(ram: Vec<usize>) -> Self {
-		let mut computer = Computer::from(ram);
-		computer.run();
-		computer
-	}
-
-	//resets the the memory and instruction pointer
-	pub fn reset(&mut self) {
-		self.ip = 0;
-		self.memory = self.rom.clone();
-	}
-
-	// runs the rom code
-	pub fn run(&mut self) {
-		loop {
-			let opcode = self.memory[self.ip];
-			if opcode == 99 {
-				break;
-			}
-
-			let src1 = self.memory[self.ip + 1];
-			let src2 = self.memory[self.ip + 2];
-			let dst = self.memory[self.ip + 3];
-
-			let par1 = self.memory[src1];
-			let par2 = self.memory[src2];
-
-			match opcode {
-				1 => self.memory[dst] = par1 + par2,
-				2 => self.memory[dst] = par1 * par2,
-				e => panic!("Error: {} no valid opcode", e),
-			}
-
-			self.ip += 4;
-		}
-	}
-
-	// get a value in memory
-	pub fn get(&self, pos: usize) -> usize {
-		self.memory[pos]
-	}
-
-	// set a value in memory
-	pub fn set(&mut self, pos: usize, val: usize) {
-		self.memory[pos] = val;
-	}
-
-	// testing purpose only
-	#[allow(unused)]
-	pub fn mem(&self) -> &[usize] {
-		&self.memory
-	}
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-	fn test_computer_run() {
-		assert_eq!(
-			Computer::run_memory(vec![1, 0, 0, 0, 99]).mem(),
-			&[2, 0, 0, 0, 99]
-		);
-		assert_eq!(
-			Computer::run_memory(vec![2, 3, 0, 3, 99]).mem(),
-			&[2, 3, 0, 6, 99]
-		);
-		assert_eq!(
-			Computer::run_memory(vec![2, 4, 4, 5, 99, 0]).mem(),
-			&[2, 4, 4, 5, 99, 9801]
-		);
-		assert_eq!(
-			Computer::run_memory(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]).mem(),
-			&[30, 1, 1, 4, 2, 5, 6, 0, 99]
-		);
-	}
 }
